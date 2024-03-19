b56f33b059fa6e85e523d983652c8f8522c672c3
Al_P <al.e.poliakow@gmail.com>
2024-03-19 11:09:12 +1100

    Added to skills and projects

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const gtwButton = document.querySelector(".gtw button");
+
+gtwButton.addEventListener("click", function(){
+    location.href="file:///C:/Users/alepo/Documents/Coding/guess-the-word/index.html";
+//url to be updated
+});
+
